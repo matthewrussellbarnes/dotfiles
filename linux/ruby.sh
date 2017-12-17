74ce650b559f9850c3979fbebaa1044e0928dab3
Joshua Balloch <joshuaballoch@gmail.com>
2017-12-17 13:05:10 +0000

    Updates to ruby

--- linux/ruby.sh
+++ linux/ruby.sh
@@ -1,15 +1,27 @@
+## TMP DIRS
+TMP_DIR=~/install_tmp
+mkdir -p "$TMP_DIR" && cd "$TMP_DIR"
+
+echo "installing chruby"
 # Install chruby:
-cd
 wget -O chruby-0.3.9.tar.gz https://github.com/postmodern/chruby/archive/v0.3.9.tar.gz
 tar -xzvf chruby-0.3.9.tar.gz
 cd chruby-0.3.9/
 sudo make install
 
+echo "install of chruby complete"
+
+cd "$TMP_DIR"
+
+echo "installing ruby-install"
 # Install ruby-install:
-cd
 wget -O ruby-install-0.6.1.tar.gz https://github.com/postmodern/ruby-install/archive/v0.6.1.tar.gz
 tar -xzvf ruby-install-0.6.1.tar.gz
 cd ruby-install-0.6.1/
 sudo make install
+
+echo "install of ruby-install complete"
+
+cd ~/ && rm -rf "$TMP_DIR"
 
 ruby-install ruby 2.4.2
