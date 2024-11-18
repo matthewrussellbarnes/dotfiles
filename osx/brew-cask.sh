2c9a23effb2c08987a9899d74116f39e6d2903bd
Matt Barnes <mbarnes2510@googlemail.com>
2024-11-18 13:47:28 +0000

    add other casks I forgot

--- osx/brew-cask.sh
+++ osx/brew-cask.sh
@@ -4,16 +4,20 @@ apps=(
    1password
    anaconda
    anki
+   brave-browser
    discord
-   google-chrome
    karabiner-elements
+   microsoft-excel
+   microsoft-powerpoint
    microsoft-teams
+   microsoft-word
    slack
    spotify
    private-internet-access
    visual-studio-code
    vlc
    whatsapp
+   zotero
 )
 
 # Install packages, attempt upgrade if already installed
